6a4feff9938dd862849d5fb0b43cabdea77a36eb
Nick Adams <22064078+nickkadams@users.noreply.github.com>
2024-02-26 10:27:50 -0500

    Update demo.sh output

--- app/stacks/karpenter/demo/demo.sh
+++ app/stacks/karpenter/demo/demo.sh
@@ -3,46 +3,51 @@
 ### Initial deployment
 kubectl apply -f demo-deployment.yaml
 echo ""
-kubectl get nodes --sort-by=.metadata.creationTimestamp
+# kubectl get nodes --sort-by=.metadata.creationTimestamp
+kubectl get nodes -o json | jq -Cjr '.items[] | .metadata.name," ",.metadata.labels."beta.kubernetes.io/instance-type"," ",.metadata.labels."eks.amazonaws.com/nodegroup"," ",.metadata.creationTimestamp, "\n"' | sort -k4
 echo ""
 kubectl get pods --sort-by=.metadata.creationTimestamp
 echo ""
 
 ### Scale out
 echo "Scale out to 30 pods"
+echo ""
 kubectl scale --replicas=30 deployment/demo-deployment
-sleep 45
+sleep 50
 echo ""
 kubectl get pods --sort-by=.metadata.creationTimestamp
 echo ""
-kubectl get nodes --sort-by=.metadata.creationTimestamp
+kubectl get nodes -o json | jq -Cjr '.items[] | .metadata.name," ",.metadata.labels."beta.kubernetes.io/instance-type"," ",.metadata.labels."eks.amazonaws.com/nodegroup"," ",.metadata.creationTimestamp, "\n"' | sort -k4
 echo ""
 
 ### Scale more
 echo "Scale out to 45 pods"
+echo ""
 kubectl scale --replicas=45 deployment/demo-deployment
-sleep 45
+sleep 50
 echo ""
 kubectl get pods --sort-by=.metadata.creationTimestamp
 echo ""
-kubectl get nodes --sort-by=.metadata.creationTimestamp
+kubectl get nodes -o json | jq -Cjr '.items[] | .metadata.name," ",.metadata.labels."beta.kubernetes.io/instance-type"," ",.metadata.labels."eks.amazonaws.com/nodegroup"," ",.metadata.creationTimestamp, "\n"' | sort -k4
 echo ""
 
 ### Scale more more
 echo "Scale out to 100 pods"
+echo ""
 kubectl scale --replicas=100 deployment/demo-deployment
-sleep 45
+sleep 50
 echo ""
 kubectl get pods --sort-by=.metadata.creationTimestamp
 echo ""
-kubectl get nodes --sort-by=.metadata.creationTimestamp
+kubectl get nodes -o json | jq -Cjr '.items[] | .metadata.name," ",.metadata.labels."beta.kubernetes.io/instance-type"," ",.metadata.labels."eks.amazonaws.com/nodegroup"," ",.metadata.creationTimestamp, "\n"' | sort -k4
 echo ""
 
 ### Scale in
 echo "Scale in to 1 pod"
-kubectl scale --replicas=1 deployment/demo-deployment
-sleep 45
 echo ""
-kubectl get nodes --sort-by=.metadata.creationTimestamp
+kubectl scale --replicas=1 deployment/demo-deployment
+sleep 75
+echo ""
+kubectl get nodes -o json | jq -Cjr '.items[] | .metadata.name," ",.metadata.labels."beta.kubernetes.io/instance-type"," ",.metadata.labels."eks.amazonaws.com/nodegroup"," ",.metadata.creationTimestamp, "\n"' | sort -k4
 echo ""
 kubectl get pods --sort-by=.metadata.creationTimestamp
